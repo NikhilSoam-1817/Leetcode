d95eb76595cee98145915b0e78eb7c9c6673f66d
NikhilSoam-1817 <soamnikhil64@gmail.com>
2026-01-18 11:10:49 +0530

    Time: 1 ms (77.43%) | Memory: 42.5 MB (38.41%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -2,12 +2,12 @@ class Solution {
     public boolean isHappy(int n) {
       
       Set <Integer> set=new HashSet<>();
-      set.add(n);
+      
       while(true){
        
         int sum=0;
         while(n!=0){
-            int digit=n%10;
+           int digit=n%10;
            sum=sum+(digit*digit);
            n=n/10;
         }
